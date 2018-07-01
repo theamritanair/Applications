f941179efcb37e8792f4e6bb87e22b58d808c0e7
Amrita <theamritanair@gmail.com>
2018-07-01 11:55:20 +0530

    Order summary added

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -25,43 +25,49 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int price = quantity*5;
-       String priceMessage = "Total: $"+price;
-        priceMessage= priceMessage + "\n Thank you.";
-        displayMessage(priceMessage);
+        int price = calculatePrice(quantity);
+      String summaryMessage = createOrderSummary(price);
+      displayMessage(summaryMessage);
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
     /**
      * This method displays the given price on the screen.
      */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
 
     public void increment(View view){
 
         quantity+=1;
-        display(quantity);
+        displayQuantity(quantity);
 
     }
     public void decrement(View view){
         quantity-=1;
-        display(quantity);
+        displayQuantity(quantity);
 
     }
     /**
      * This method displays the given text on the screen.
      */
     private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
+    }
+    public int calculatePrice(int quantity){
+        int price = quantity*5;
+        return price;
+    }
+
+    public String createOrderSummary(int price){
+        String summaryMessage = "Name: Alaska O'Neal";
+       summaryMessage = summaryMessage + "\n Quantity: "+ quantity;
+       summaryMessage = summaryMessage +"\n Total: $" +price +"\n Thank You!";
+       return summaryMessage;
     }
 }
