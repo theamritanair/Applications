020ea356e8ec918b8d1924cc66323deefbf58772
Amrita <theamritanair@gmail.com>
2018-07-01 21:35:32 +0530

    Opens up Email

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -2,11 +2,17 @@ package com.example.android.justjava;
 
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.net.URI;
 import java.text.NumberFormat;
 
 /**
@@ -25,9 +31,31 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int price = calculatePrice(quantity);
-      String summaryMessage = createOrderSummary(price);
-      displayMessage(summaryMessage);
+
+        EditText editText = (EditText) findViewById(R.id.name_field);
+        String name = editText.getText().toString();
+
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+
+
+        CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        boolean hasChocolate = chocolateCheckBox.isChecked();
+
+        int price = calculatePrice(hasWhippedCream,hasChocolate);
+
+      String summaryMessage = createOrderSummary(name,price,hasWhippedCream,hasChocolate);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Just Java to " + name);
+        intent.putExtra(Intent.EXTRA_TEXT, summaryMessage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+        displayMessage(summaryMessage);
+
+
     }
 
     /**
@@ -42,12 +70,24 @@ public class MainActivity extends AppCompatActivity {
      */
 
     public void increment(View view){
+        if (quantity == 100) {
+            // Show an error message as a toast
+            Toast.makeText(this, "You cannot have more than 100 coffees", Toast.LENGTH_SHORT).show();
+            // Exit this method early because there's nothing left to do
+            return;
+        }
 
         quantity+=1;
         displayQuantity(quantity);
 
     }
     public void decrement(View view){
+        if (quantity == 1) {
+            // Show an error message as a toast
+            Toast.makeText(this, "You cannot have less than 1 coffee", Toast.LENGTH_SHORT).show();
+            // Exit this method early because there's nothing left to do
+            return;
+        }
         quantity-=1;
         displayQuantity(quantity);
 
@@ -59,15 +99,24 @@ public class MainActivity extends AppCompatActivity {
         TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
         orderSummaryTextView.setText(message);
     }
-    public int calculatePrice(int quantity){
-        int price = quantity*5;
+    public int calculatePrice(boolean addWhippedCream, boolean addChocolate){
+        int basePrice =5;
+        if(addWhippedCream){
+            basePrice+=1;
+        }
+        if(addChocolate){
+            basePrice+=2;
+        }
+        int price = basePrice*quantity;
         return price;
     }
 
-    public String createOrderSummary(int price){
-        String summaryMessage = "Name: Alaska O'Neal";
-       summaryMessage = summaryMessage + "\n Quantity: "+ quantity;
-       summaryMessage = summaryMessage +"\n Total: $" +price +"\n Thank You!";
+    public String createOrderSummary(String name, int price,boolean addWhippedCream, boolean addChocolate){
+        String summaryMessage = "Name: " +name;
+        summaryMessage+= "\nWhipped Cream: " + addWhippedCream;
+        summaryMessage+="\nChocolate: " +addChocolate;
+       summaryMessage = summaryMessage + "\nQuantity: "+ quantity;
+       summaryMessage = summaryMessage +"\nTotal: $" +price +"\n Thank You!";
        return summaryMessage;
     }
 }
