5dee04e0fe154949d15c56d2916fdb2c231c3595
Amrita <theamritanair@gmail.com>
2018-07-16 11:02:50 +0530

    Implemented ButterKnife

--- Cookies/app/src/main/java/com/example/android/cookies/MainActivity.java
+++ Cookies/app/src/main/java/com/example/android/cookies/MainActivity.java
@@ -6,23 +6,33 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.android_cookie_image_view)
+    ImageView imageView;
+
+    @BindView(R.id.status_text_view)
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 
     /**
      * Called when the cookie should be eaten.
      */
     public void eatCookie(View view) {
-        ImageView imageView = (ImageView) findViewById(R.id.android_cookie_image_view);
+//        ImageView imageView = (ImageView) findViewById(R.id.android_cookie_image_view);
         imageView.setImageResource(R.drawable.after_cookie);
         // TODO: Find a reference to the ImageView in the layout. Change the image.
-        TextView textView = (TextView) findViewById(R.id.status_text_view);
+//        TextView textView = (TextView) findViewById(R.id.status_text_view);
         textView.setText("I'm so full");
         // TODO: Find a reference to the TextView in the layout. Change the text.
 
